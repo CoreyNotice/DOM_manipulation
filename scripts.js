cb60bb36a8702d71c318c277fc10e45051ab51d9
Corey N <coreynotice@yahoo.com>
2023-03-23 18:36:32 -0400

    change attributes

--- scripts.js
+++ scripts.js
@@ -4,10 +4,12 @@ const div = document.querySelector("div")
 const spanHi = document.querySelector("#hi") 
 const spanBye = document.querySelector("#bye") 
 
-console.log(spanHi.getAttribute("id"))
-console.log(spanHi.getAttribute("title"))
+console.log(spanHi.setAttribute("id","yoo baby yo"))
+
+console.log(spanHi.setAttribute("title","yoo baby yo"))
 
 // or
-console.log(spanHi.id)
-console.log(spanHi.title)
+spanHi.id="yoo babe"
+
+
 
